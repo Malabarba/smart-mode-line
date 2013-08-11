bf4720dd762a1cb12e29fc0c2ee9e45fe29205d3
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-11 14:14:55 +0100

    Fix for the erc fix.

--- smart-mode-line.el
+++ smart-mode-line.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 ;; URL: http://github.com/Bruce-Connor/smart-mode-line
-;; Version: 1.28
+;; Version: 1.28.1
 ;; Keywords: faces frames
 
 ;;; Commentary:
@@ -144,6 +144,7 @@
 ;; 
 
 ;;; Change Log:
+;; 1.28.1 - 20130811 - Fix for the erc fix.
 ;; 1.28 - 20130811 - Fixing erc notifications.
 ;; 1.27 - 20130810 - Changed default value of sml/mode-width to a number. 'full didn't work for everyone.
 ;; 1.27 - 20130810 - Doc bug.
@@ -205,9 +206,9 @@
 
 ;; (eval-when-compile (require 'cl))
 
-(defconst sml/version "1.28" "Version of the smart-mode-line.el package.")
+(defconst sml/version "1.28.1" "Version of the smart-mode-line.el package.")
 
-(defconst sml/version-int 30 "Version of the smart-mode-line.el package, as an integer.")
+(defconst sml/version-int 31 "Version of the smart-mode-line.el package, as an integer.")
 
 (defun sml/bug-report ()
   "Opens github issues page in a web browser. Please send me any bugs you find, and please inclue your emacs and sml versions."
@@ -907,9 +908,9 @@ Might implement a quick flash eventually."
     (eval-after-load "nyan-mode"
       '(sml/nyan-support))
 
-    (unless (boundp erc-track-position-in-mode-line)
-      (when erc-track-position-in-mode-line
-       (setq erc-track-position-in-mode-line t)))))
+    (unless (and (boundp erc-track-position-in-mode-line)
+                 (null erc-track-position-in-mode-line))
+      (setq erc-track-position-in-mode-line t))))
 
 (defun sml/sml-modeline-support ()
   "Create a variable regarding `sml-modeline-mode' and insert `sml-modeline-create' in one of the anchors."
